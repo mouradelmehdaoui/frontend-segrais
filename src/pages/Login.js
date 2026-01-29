c5a8f26a166159d0eea2d2f7e61d798be0696db2
mouradelmehdaoui <mourad.elmehdaoui93@gmail.com>
2026-01-29 11:51:42 +0100

    fix login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await axios.post("https://backend-segrais.onrender.com//api/auth/login", {
+      const res = await axios.post("https://backend-segrais.onrender.com/api/auth/login", {
         login,
         password
       });
