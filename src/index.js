8c0daa35062c1d509b7797455f3cbea01c704f13
mouradelmehdaoui <mourad.elmehdaoui93@gmail.com>
2026-01-29 01:57:45 +0100

    fix bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
